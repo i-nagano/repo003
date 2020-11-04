56fb4a5dbdd273d56a9760f120062a24ba942aca
githubb i.nagano78 <github i.nagano78@gmail.com>
2020-11-04 15:22:28 +0900

    2020/11/04 added.

--- resources/js/components/routerviews/Members.vue
+++ resources/js/components/routerviews/Members.vue
@@ -1,14 +1,43 @@
 <template>
-    <div>
+  <div>
+    <v-container class="mt-12">
+      <v-row class="mt-6" justify="start">
         <h2>Members Test</h2>
-    </div>
+        <label for="name">名前:</label>
+        <input type="text" name="name" id="name" v-model="name" />
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email" v-model="email">
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Members',
-
-}
+  name: "Members",
+  components: {},
+  data: function() {
+      return {
+          name: '',
+          email: '',
+          members: [],
+      };
+  },
+  methods: {
+      getMember() {
+          axios
+          .get('/members')
+          .then(response =>{
+              this.members = response.data;
+              return console.log(response.data);
+              })
+          .catch(error =>{
+             this.message = error.response.data;
+             return console.log(error.response.data);
+            })
+      }
+  },
+};
 </script>
 
 <style lang="" scoped>
